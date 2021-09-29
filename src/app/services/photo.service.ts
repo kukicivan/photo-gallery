3af0ff0c7b8ebba783d3741d2f89209d056d3da7
Ivan Kukic <ivan.kukic@gmail.com>
2021-09-29 19:08:41 +0200

    update photo service

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraPhoto, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 
+export interface PhotoStore {
+  filepath: string;
+  webviewPath: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -19,16 +23,18 @@ export class PhotoService {
     reader.readAsDataURL(blob);
   });
 
+  // eslint-disable-next-line @typescript-eslint/member-ordering
   constructor() {
   }
 
   public async addNewToGallery() {
     // Take a photo
-    const capturedPhoto = await Camera.getPhoto({
+    const photoOption = {
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
-    });
+    };
+    const capturedPhoto = await Camera.getPhoto(photoOption);
 
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
@@ -36,9 +42,10 @@ export class PhotoService {
 
   }
 
-  private async savePicture(cameraPhoto: CameraPhoto): Promise<PhotoStore> {
+  private async savePicture(cameraPhoto: any): Promise<PhotoStore> {
     // Convert photo to base64 format, required by Filesystem API to save
     const base64Data = await this.readAsBase64(cameraPhoto);
+
 
     // Write the file to the data directory
     const fileName = new Date().getTime() + '.jpeg';
@@ -48,26 +55,24 @@ export class PhotoService {
       directory: Directory.Data
     });
 
+    console.log('savedFile', savedFile);
+
     // Use webPath to display the new image instead of base64 since it's
     // already loaded into memory
-    return {
+    const inMemory = {
       filepath: fileName,
       webviewPath: cameraPhoto.webPath
     };
+
+    return inMemory;
   }
 
-  private async readAsBase64(cameraPhoto: CameraPhoto) {
+  private async readAsBase64(cameraPhoto: any) {
     // Fetch the photo, read as a blob, then convert to base64 format
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const response = await fetch(cameraPhoto.webPath!);
+    const response = await fetch(cameraPhoto?.webPath);
     const blob = await response.blob();
 
     return await this.convertBlobToBase64(blob) as string;
   }
 
-}
-
-export interface PhotoStore {
-  filepath: string;
-  webviewPath: string;
 }
